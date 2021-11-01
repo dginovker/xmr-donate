7f34624e9939b4502a80df6fe821a8b5d964a0f3
dginovker <danielginovker@gmail.com>
2021-10-31 21:22:15 -0400

    Add terms

--- src/pages/Terms/App.vue
+++ src/pages/Terms/App.vue
@@ -1,21 +1,63 @@
+ 
 <template>
-  xmr-donates does not endorse any project it links here. This website is simply
-  a refined collection of notable projects that accept Monero. Requirements to
-  be added to this website are loosely the following: Project is ANY of: *
-  Project contributes to Monero in some way * Project is FOSS * Project
-  advocates or enhances privacy in some way Project MUST be: * Somewhat active *
-  Somewhat noteworthy * Open source (if there's an app)
+  <div>
+    <flow-form v-bind:questions="questions">
+      <a href="/">xmr-donates</a> does not endorse any project it links here.
+      This website is simply a refined collection of notable projects that
+      accept Monero.
+      <br />
+      <br />
+      Requirements to be added to this website are loosely the following:
+      <br />
+      <br />
+      Project is ANY of:
+      <ul>
+        <li>Project contributes to Monero in some way</li>
+        <li>Project is FOSS</li>
+        <li>Project advocates or enhances privacy in some way</li>
+      </ul>
 
+      Project MUST be:
+      <ul>
+        <li>Somewhat active</li>
+        <li>Somewhat noteworthy</li>
+        <li>Open source (if there's an app)</li>
+      </ul>
+
+      Contributors:
+      <ul>
+        <li>
+          Dan Ginovker (<a href="https://github.com/dginovker">Github</a>)
+        </li>
+      </ul>
+
+      <br />
+      <p class="description">
+        <span><a href="./">Back</a></span>
+      </p>
+    </flow-form>
+  </div>
   <footer class="vff-footer" style="left: 0.6rem; bottom: 0.4rem">
     <a href="https://github.com/dginovker/xmr-donate/issues">Feedback</a> |
     <a href="https://github.com/dginovker/xmr-donate">Source</a> |
-    <a href="https://github.com/dginovker/xmr-donate">Terms</a>
+    <a href="./terms.html">Terms</a>
   </footer>
 </template>
 
+
 <script>
+import { FlowForm } from "@ditdot-dev/vue-flow-form";
+
 export default {
-  name: "Terms"
+  name: "Donate Monero",
+  components: {
+    FlowForm,
+  },
+  data() {
+    return {
+      questions: [],
+    };
+  },
 };
 </script>
 
